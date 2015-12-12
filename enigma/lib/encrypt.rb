d6db58ad57958a20aead624ed2161710f5922edb
Adrienne Domingus <adrienne.domingus@gmail.com>
2015-12-12 06:15:28 -0700

    Update rotator methods and tests

--- enigma/lib/encrypt.rb
+++ enigma/lib/encrypt.rb
@@ -1,7 +1,9 @@
 class Encrypt
   attr_accessor :characters_and_indices
-  # def initialize
-  # end
+  def initialize(key, date)
+    @key = key
+    @date = date
+  end
 
   def character_chart
     characters = []
@@ -17,31 +19,40 @@ class Encrypt
   end
 
   def date_offset
-    date = Time.now.strftime("%d%m%y").to_i
-    date_offset = date ** 2
-    @date_key = date_offset.to_s.split("")[-4..-1].join
+    @date = Time.now.strftime("%d%m%y").to_i
+    offset = @date ** 2
+    @date_key = offset.to_s.split("")[-4..-1].join
+    @date_key
   end
 
   def date_rotation
-    @a_date_rotation = @date_key[0].to_i
-    @b_date_rotation = @date_key[1].to_i
-    @c_date_rotation = @date_key[2].to_i
-    @d_date_rotation = @date_key[3].to_i
+    date_offset
+    a_date_rotation = @date_key[0].to_i
+    b_date_rotation = @date_key[1].to_i
+    c_date_rotation = @date_key[2].to_i
+    d_date_rotation = @date_key[3].to_i
+    @date_rotations = [a_date_rotation, b_date_rotation, c_date_rotation, d_date_rotation]
+    @date_rotations
   end
 
   def key_rotation
-    key = Random.rand(0..99999).to_s
-    @a_key_rotation = key[0..1].to_i
-    @b_key_rotation = key[1..2].to_i
-    @c_key_rotation = key[2..3].to_i
-    @d_key_rotation = key[3..4].to_i
+    a_key_rotation = @key.to_s[0..1].to_i
+    b_key_rotation = @key.to_s[1..2].to_i
+    c_key_rotation = @key.to_s[2..3].to_i
+    d_key_rotation = @key.to_s[3..4].to_i
+    @key_rotations = [a_key_rotation, b_key_rotation, c_key_rotation, d_key_rotation]
+    @key_rotations
   end
 
   def overall_rotation
-    @a_overall_rotation = @a_date_rotation + @a_key_rotation
-    @b_overall_rotation = @a_date_rotation + @a_key_rotation
-    @c_overall_rotation = @a_date_rotation + @a_key_rotation
-    @d_overall_rotation = @a_date_rotation + @a_key_rotation
+    date_rotation
+    key_rotation
+    a_overall_rotation = @date_rotations[0] + @key_rotations[0]
+    b_overall_rotation = @date_rotations[1] + @key_rotations[1]
+    c_overall_rotation = @date_rotations[2] + @key_rotations[2]
+    d_overall_rotation = @date_rotations[3] + @key_rotations[3]
+    @overall_rotations = [a_overall_rotation, b_overall_rotation, c_overall_rotation, d_overall_rotation]
+    @overall_rotations
   end
 
   def map_letter(letter)
@@ -83,3 +94,9 @@ class Encrypt
   end
 
 end
+
+# static_date = Time.new 1983, 3, 14
+# e = Encrypt.new(Random.rand(0..99999).to_s)
+# e = Encrypt.new("52341")
+# e = Encrypt.new(Time.now.strftime("%d%m%y").to_i))
+# e = Encrypt.new(static_date)
