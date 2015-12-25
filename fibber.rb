5707c0938fe38230fc295e4acbcf003adb0960ae
Adrienne Domingus <adrienne.domingus@gmail.com>
2015-12-25 12:26:18 -0700

    add line separation

--- fibber.rb
+++ fibber.rb
@@ -1,14 +1,26 @@
 # https://github.com/turingschool/challenges/blob/master/fibber.markdown
 
 def fibbonacci(quantity)
+  sequence = []
   first = 0
   second = 1
-  puts first
-  puts second
+  sequence << first.to_s + " "
+  sequence << second.to_s + " "
   (quantity - 2).times do
-    puts first + second
+    sequence << (first + second).to_s + " "
     second, first = first + second, second
   end
+  sequence = output(sequence)
+  sequence.join
+end
+
+def output(sequence)
+  n = 5
+  while n < sequence.length
+    sequence.insert(n, "\n")
+    n += 6
+  end
+  sequence
 end
 
 puts fibbonacci(25)
