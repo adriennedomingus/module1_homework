abcfd4bc8205acc15608d99605aa3aaff8433b32
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-06-29 11:50:25 -0700

    Both headers rendering

---
+++ creact-starter/app/assets/javascripts/components/_all_skills.js.jsx
@@ -0,0 +1,9 @@
+var AllSkills = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>AllSkills</h1>
+      </div>
+    )
+  }
+})
---
+++ creact-starter/app/assets/javascripts/components/_new_skill.js.jsx
@@ -0,0 +1,9 @@
+var NewSkill = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    )
+  }
+})
